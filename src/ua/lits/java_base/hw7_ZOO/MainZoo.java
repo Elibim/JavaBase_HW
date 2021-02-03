e644dfcf90ef06836468f0cb11147912f6687186
Elibim <elibielska@gmail.com>
2021-02-03 17:19:43 +0200

    hw15 changes

--- src/ua/lits/java_base/hw7_ZOO/MainZoo.java
+++ src/ua/lits/java_base/hw7_ZOO/MainZoo.java
@@ -107,39 +107,77 @@ public class MainZoo {
         accountant.income();
 
         System.out.println("\n***********************");
-        System.out.println("Please enter No of food for animal you want to feed:" +
-                "\n11 meat for Alpaca" +
-                "\n12 meat for Wolf +" +
-                "\n13 meat for Hedgehog +" +
-                "\n14 meat for Peacock +" +
-                "\n15 meat for Toucan" +
-                "\n16 meat for Eagle +" +
-                "\n21 mix food for Alpaca" +
-                "\n22 mix food for Wolf" +
-                "\n23 mix food for Hedgehog +" +
-                "\n24 mix food for Peacock +" +
-                "\n25 mix food for Toucan" +
-                "\n26 mix food for Eagle" +
-                "\n31 plants for Alpaca +" +
-                "\n32 plants for Wolf" +
-                "\n33 plants for Hedgehog +" +
-                "\n34 plants for Peacock +" +
-                "\n35 plants for Toucan +" +
-                "\n36 plants for Eagle");
 
-        try {
-            Scanner feedingAnimals = new Scanner(System.in);
-            int foodAnimal = feedingAnimals.nextInt();
-            ArrayList<Integer> list = new ArrayList<>(List.of(12,13,14,16,23,24,31,33,34,35));
-                if (list.contains(foodAnimal)) {
-                    throw new Exception("Hurray! Now animal is not hungry!");
-                }
-                else {
-                    throw new Exception("This animal is not eating such a food or you insert wrong number!");
-                }
+        Scanner foodType = new Scanner(System.in);
+        boolean exit = false;
+
+        Feeding feeding = new Feeding();
+
+        while (!exit) {
+            System.out.println("Please choose food type:" +
+                    "\n1 " + FoodType.carnivorous +
+                    "\n2 " + FoodType.omnivorous +
+                    "\n3 " + FoodType.herbivores +
+                    "\n0 exit");
+            int food = foodType.nextInt();
+            switch (food) {
+                case 1:
+                    System.out.println("You choose " + FoodType.carnivorous + " food type");
+                    feeding.giveFood(FoodType.carnivorous);
+                    break;
+                case 2:
+                    System.out.println("You choose " + FoodType.omnivorous + " food type");
+                    feeding.giveFood(FoodType.omnivorous);
+                    break;
+                case 3:
+                    System.out.println("You choose " + FoodType.herbivores + " food type");
+                    feeding.giveFood(FoodType.herbivores);
+                    break;
+                case 0:
+                    System.out.println("Bye!");
+                    exit = true;
+                    break;
+
+            }
+
         }
-        catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+
+
+
+
+//        System.out.println("Please enter No of food for animal you want to feed:" +
+//                "\n11 meat for Alpaca" +
+//                "\n12 meat for Wolf +" +
+//                "\n13 meat for Hedgehog +" +
+//                "\n14 meat for Peacock +" +
+//                "\n15 meat for Toucan" +
+//                "\n16 meat for Eagle +" +
+//                "\n21 mix food for Alpaca" +
+//                "\n22 mix food for Wolf" +
+//                "\n23 mix food for Hedgehog +" +
+//                "\n24 mix food for Peacock +" +
+//                "\n25 mix food for Toucan" +
+//                "\n26 mix food for Eagle" +
+//                "\n31 plants for Alpaca +" +
+//                "\n32 plants for Wolf" +
+//                "\n33 plants for Hedgehog +" +
+//                "\n34 plants for Peacock +" +
+//                "\n35 plants for Toucan +" +
+//                "\n36 plants for Eagle");
+//
+//        try {
+//            Scanner feedingAnimals = new Scanner(System.in);
+//            int foodAnimal = feedingAnimals.nextInt();
+//            ArrayList<Integer> list = new ArrayList<>(List.of(12,13,14,16,23,24,31,33,34,35));
+//                if (list.contains(foodAnimal)) {
+//                    throw new Exception("Hurray! Now animal is not hungry!");
+//                }
+//                else {
+//                    throw new Exception("This animal is not eating such a food or you insert wrong number!");
+//                }
+//        }
+//        catch (Exception e) {
+//            System.out.println(e.getMessage());
+//        }
     }
 }
